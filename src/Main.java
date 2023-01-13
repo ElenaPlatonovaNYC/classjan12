04d07cf0401c80215a54c333790d45bd5f503ca4
fiestalifeofelena <fiesta@MacBook-Air-FIESTA.local>
2023-01-12 20:30:48 -0500

    pushing code from class today

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int worldCup=2022;
+        switch(worldCup){
+            default:
+                System.out.println("I dont know who is the winner");
+                break;
+            case 2022:
+                System.out.println("winner is Argentina");
+                break;
+            case 2018:
+                System.out.println("winner is France");
+                break;
+            case 2014:
+                System.out.println("winner is Germany");
+                break;
+
+        }
+
+
+    }
+}
